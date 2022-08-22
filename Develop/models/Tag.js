0206eb6a2becec532e793bc3155ccfdc8e5ef49c
SSullivan <spencersullivan1990@gmail.com>
2022-08-21 20:07:42 -0500

    tag model typed up

--- Develop/models/Tag.js
+++ Develop/models/Tag.js
@@ -1,12 +1,21 @@
 const { Model, DataTypes } = require('sequelize');
-
+const Product = require('./Product');
 const sequelize = require('../config/connection.js');
+const ProductTag = require('./ProductTag');
 
 class Tag extends Model {}
 
 Tag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    tag_name: {
+      type: DataTypes.String
+    }
   },
   {
     sequelize,
