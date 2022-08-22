b5a1bb7d94d06e444aea0fc4dfc0868bbfb9b009
SSullivan <spencersullivan1990@gmail.com>
2022-08-22 10:26:13 -0500

    product routes typed up

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -7,12 +7,27 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: Category, Tag
+  })
+    ,then(data => {
+      res.send(data)
+    })
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: Category, Tag
+  })
+  .then(data => {
+    res.send(data)
+  })
 });
 
 // create new product
@@ -91,6 +106,14 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+  .then(() => {
+    res.status(200).json('product was destroyed')
+  })
 });
 
 module.exports = router;
